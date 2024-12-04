bcc067a154883ad5cb3e60678fbb35b8a0e44152
Jack Wagner <wagjack@umich.edu>
2024-12-04 13:33:48 -0500

    implemented alpha beta pruning
    adjusted minimax.py to include alpha beta pruning to decrease performance strain on tic tac toe implementation

--- minimax.py
+++ minimax.py
@@ -8,49 +8,67 @@ def minimax(board):
     curr = ttt.current_player(board)
 
     if curr == c.X: #X will be the max player (find max of min_value())
-        score = float('-inf')
+        score = c.MIN
         optimal_move = None
+        alpha = c.MIN
+        beta = c.MAX
 
         for a in ttt.actions(board):
-            val = min_value(ttt.resultant(board, a))
+            val = min_value(ttt.resultant(board, a), alpha, beta)
 
             if val > score:
                 score = val
                 optimal_move = a
 
+            alpha = max(alpha, score)
+            if alpha >= beta:
+                break
+
     else: #O will be the min player (fid min of max_value())
-        score = float('inf')
+        score = c.MAX
         optimal_move = None
+        alpha = c.MIN
+        beta = c.MAX
 
         for a in ttt.actions(board):
-            val = max_value(ttt.resultant(board, a))
+            val = max_value(ttt.resultant(board, a), alpha, beta)
 
             if val < score:
                 score = val
                 optimal_move = a
 
+            beta = min(beta, score)
+            if alpha >= beta:
+                break
+
     return optimal_move
 
 
-def min_value(board):
+def max_value(board, alpha, beta):
     if ttt.terminal(board):
         return ttt.result(board)
     
-    v = float('inf')
+    v = c.MIN
 
     for a in ttt.actions(board):
-        v = min(v, max_value(ttt.resultant(board, a)))
+        v = max(v, min_value(ttt.resultant(board, a), alpha, beta))
+        alpha = max(alpha, v)
+        if alpha >= beta:
+            break
 
     return v
 
 
-def max_value(board):
+def min_value(board, alpha, beta):
     if ttt.terminal(board):
         return ttt.result(board)
     
-    v = float('-inf')
+    v = c.MAX
 
     for a in ttt.actions(board):
-        v = max(v, min_value(ttt.resultant(board, a)))
+        v = min(v, max_value(ttt.resultant(board, a), alpha, beta))
+        beta = min(beta, v)
+        if alpha >= beta:
+            break
 
     return v
